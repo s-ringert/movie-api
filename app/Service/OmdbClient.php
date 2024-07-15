fcecbb771ee9fa65cc72b19b35d09e600ea7e93c
Sascha Ringert <sascha@ringert.de>
2024-07-16 00:53:14 +0200

    update githubb actions

--- app/Service/OmdbClient.php
+++ app/Service/OmdbClient.php
@@ -7,8 +7,6 @@ use GuzzleHttp\Client;
 class OmdbClient
 {
     private const API_BASE_URL = 'https://omdbapi.com';
-    public function __construct(private readonly Client $client, private readonly string $omdbApiKey)
-    {
-    }
 
+    public function __construct(private readonly Client $client, private readonly string $omdbApiKey) {}
 }
