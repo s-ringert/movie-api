253cd6a3fadea465914538e90e3eba935d26c232
Sascha Ringert <sascha@ringert.de>
2024-07-28 15:39:16 +0200

    add omdbclient Test

--- tests/Unit/Service/OmdbClientTest.php
+++ tests/Unit/Service/OmdbClientTest.php
@@ -2,12 +2,66 @@
 
 namespace Tests\Unit\Service;
 
-use PHPUnit\Framework\TestCase;
+use App\Service\OmdbClient;
+use Illuminate\Support\Facades\Http;
 
-class OmdbClientTest extends TestCase
+class OmdbClientTest extends \Tests\TestCase
 {
     public function testSearchWithResult()
     {
-        $this->assertFalse(true);
+        Http::preventStrayRequests();
+        $omdbClient = new OmdbClient();
+        Http::fake([
+            '*' => Http::response([
+                'Search' => [
+                    [
+                        'Title' => 'Star Wars: Episode IV - A New Hope',
+                        'Year' => 1977
+                    ],
+                    [
+                        'Title' => 'Star Wars: Episode V - The Empire Strikes Back',
+                        'Year' => 1980
+                    ]
+                ]
+            ])
+        ]);
+
+        $result = $omdbClient->searchMovie('FOOBAR');
+
+
+        Http::assertSentCount(1);
+        $this->assertCount(2, $result);
+        $this->assertEquals(
+            [
+                [
+                    'Title' => 'Star Wars: Episode IV - A New Hope',
+                    'Year' => 1977
+                ],
+                [
+                    'Title' => 'Star Wars: Episode V - The Empire Strikes Back',
+                    'Year' => 1980
+                ]
+            ],
+            $result
+        );
+    }
+    public function testSearchWithNoResult()
+    {
+        Http::preventStrayRequests();
+        $omdbClient = new OmdbClient();
+        Http::fake([
+            '*' => Http::response()
+        ]);
+
+        $result = $omdbClient->searchMovie('FOOBAR');
+
+
+        Http::assertSentCount(1);
+        $this->assertCount(0, $result);
+        $this->assertEquals(
+            [
+            ],
+            $result
+        );
     }
 }
