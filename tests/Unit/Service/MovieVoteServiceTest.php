7b9a8d013b121bfb86e9a828df30a4d31257c2d2
Sascha Ringert <sascha@ringert.de>
2024-07-28 15:56:46 +0200

    wip MovieVoteServiceTest

--- tests/Unit/Service/MovieVoteServiceTest.php
+++ tests/Unit/Service/MovieVoteServiceTest.php
@@ -2,12 +2,30 @@
 
 namespace Tests\Unit\Service;
 
-use PHPUnit\Framework\TestCase;
+use App\Events\MovieVoted;
+use App\Models\Movie;
+use App\Models\User;
+use App\Service\MovieVoteService;
+use Illuminate\Support\Facades\Event;
+use Mockery\Mock;
+use Mockery\MockInterface;
+use Tests\TestCase;
 
 class MovieVoteServiceTest extends TestCase
 {
     public function testVote()
     {
-        $this->assertFalse(true);
+        Event::fake();
+
+        $user = $this->mock(User::class, function (MockInterface $mock) {
+            $mock->shouldReceive('attach')->once();
+            $mock->shouldReceive('save')->once();
+        });
+        $movie = $this->mock(Movie::class, function (MockInterface $mock) {});
+
+        $movieVoteService = new MovieVoteService();
+        $movieVoteService->vote($user, $movie, 3);
+
+        Event::assertDispatched(MovieVoted::class);
     }
 }
